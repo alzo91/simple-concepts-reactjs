e46c08c651e85c8045fc7e2d323ae8ef7c249e68
Alisson Zotarelli <alzo.zotarelli@gmail.com>
2021-01-20 12:59:17 -0300

    I finished to test and I'm sending to rockeseat

--- src/App.js
+++ src/App.js
@@ -1,30 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
+  const [repositories, setRepositories] = useState([]);
+  const [txtTitle, setTxtTitle] = useState("");
+  const [txtTechs, setTxtTechs] = useState("");
+
+  useEffect(() => {
+    (async function () {
+      const { data } = await api.get("repositories");
+      setRepositories([...data]);
+    })();
+  }, []);
+
+  async function handleAddRepository(e) {
+    e.preventDefault();
+
+    const newRepo = await api.post("repositories", {
+      title: txtTitle,
+      url: "https://github.com/alzo91/gostack-template-conceitos-nodejs",
+      techs: txtTechs.split(","),
+    });
+
+    console.log(newRepo);
+    setRepositories([...repositories, { ...newRepo.data }]);
+    setTxtTechs("");
+    setTxtTitle("");
     // TODO
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+    console.log("handleRemoveRepository", id);
+    await api.delete(`repositories/${id}`);
+    setRepositories(repositories.filter((repo) => repo.id !== id));
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
+    <>
+      <form onSubmit={(e) => handleAddRepository(e)}>
+        <div className="form">
+          <label htmlFor="txtRepositoryName">Nome do Repositório</label>
+          <input
+            id="txtRepositoryName"
+            type="text"
+            placeholder="Nome do Repositório"
+            value={txtTitle}
+            onChange={(e) => setTxtTitle(e.target.value)}
+          />
+          <label htmlFor="txtRepositoryTechs">Tecnologias:</label>
+          <input
+            id="txtRepositoryTechs"
+            type="text"
+            placeholder="Tecnologias separado por virgula"
+            value={txtTechs}
+            onChange={(e) => setTxtTechs(e.target.value)}
+          />
+          <button type="submit">Adicionar</button>
+        </div>
+      </form>
+      <div className="list">
+        <ul data-testid="repository-list">
+          {repositories.map((repository) => (
+            <li key={repository.id}>
+              <p>{repository.title}</p>
+              <span>{repository.techs.join(", ")}</span>
+              <button
+                className="remove"
+                onClick={() => handleRemoveRepository(repository.id)}
+              >
+                Remover
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
